79b1a506c46c32eda59f710f84d738baeda43a61
Lukáš Kovář <lkov@post.cz>
2023-08-23 20:36:02 +0200

    fix: update install.sh submodules

--- install.sh
+++ install.sh
@@ -23,6 +23,7 @@ prompt() {
 # Ask user if they want to update the repo
 if prompt "Do you want to update the repo?"; then
   git pull
+  git submodule update --init --recursive
 fi
 
 # Ask user if they want to build the project first
